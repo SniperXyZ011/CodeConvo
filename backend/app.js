55f5fc96fd604774273fc246dc636f5e3e52b8ed
Shyam Kumar <shyamprajapatiofficial2011@gmail.com>
2024-12-25 13:54:11 +0530

    Server created

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,14 @@
-
-
-import express from 'express';
+import express from "express";
+import morgan from "morgan";
 
 const app = express();
+app.use(morgan("dev"));
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-
-app.get('/', (req, res) => {
-    res.send('Welcome');
+app.get("/", (req, res) => {
+  res.send("Welcome");
 });
 
 export default app;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ dotenv.config();
 import http from 'http';
 import app from './app.js';
 
+const port = process.env.PORT || 3000;
+
 const server = http.createServer(app);
 
 
